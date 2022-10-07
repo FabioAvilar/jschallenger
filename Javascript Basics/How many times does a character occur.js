5b60ac2dd931a339ee5afe96fcef0ca637b56802
FabioBassani <avilar.fabio@yahoo.com.br>
2022-10-07 11:50:00 +0200

    a quick tipp
    you can make this too it is working;

--- Javascript Basics/How many times does a character occur.js
+++ Javascript Basics/How many times does a character occur.js
@@ -1,6 +1,36 @@
-// Write a function that takes a two strings as arguments
-// Return the number of times the first string occurs in the second string
+// Write a function that takes two strings (a and b) as arguments
+// Return the number of times a occurs in b
 
-function myFunction(pattern, match) {
-  return match.split(new RegExp('\\'+pattern, "gi" )).length - 1
+
+// function myFunction(a, b) {
+//     let convert = b.toLowerCase();
+//     let temp = 0;
+//     convert.split("").map((word) => {
+//         if (word === a) {
+//             temp += 1;
+//         }
+//     });
+//     return temp;
+// }
+
+// or we can make this
+
+function myFunction(a, b) {
+    return b.split(a).length - 1;
 }
+
+console.log(
+    myFunction("m", "how many times does the character occur in this sentence?")
+);
+
+console.log(
+    myFunction("h", "how many times does the character occur in this sentence?")
+);
+
+console.log(
+    myFunction("?", "how many times does the character occur in this sentence?")
+);
+
+console.log(
+    myFunction("z", "how many times does the character occur in this sentence?")
+);
